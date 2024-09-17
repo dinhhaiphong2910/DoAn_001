d51f203d6a440d9f2fe61ed324bfff2d07fe8533
seabreeze <teothiphong@gmail.com>
2024-09-17 15:47:52 +0700

    first commit

---
+++ src/main/java/org/example/doan_001/DoAn001Application.java
@@ -0,0 +1,13 @@
+package org.example.doan_001;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoAn001Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DoAn001Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/doan_001/DoAn001ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.doan_001;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoAn001ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
